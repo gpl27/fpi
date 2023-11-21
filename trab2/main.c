79e02ae6c92352b54a300dc8a91206e1e7149c86
gustavo lacroix <gustavolacroix@gmail.com>
2023-11-21 18:25:03 -0300

    UI Refactor

--- trab2/main.c
+++ trab2/main.c
@@ -7,16 +7,16 @@
 
 
 typedef struct {
-    gboolean grayscale;
-    GdkPixbuf *original;
-    GdkPixbuf *output;
+    GdkPixbuf *pixbuf;
     unsigned char *hist;
 } Image;
 
 typedef struct {
-    Image image;
     GtkWidget *src_window;
+    Image original_img;
+    Image img_to_match;
     GtkWidget *out_window;
+    Image output_img;
     GtkWidget *q_amount;
     GtkWidget *b_amount;
     GtkWidget *c_amount;
@@ -25,38 +25,34 @@ typedef struct {
 
 void load_image(char *filename, AppData *metadata) {
     // Unref previous data
-    if (metadata->image.original != NULL) {
-        g_object_unref(metadata->image.original);
+    if (metadata->original_img.pixbuf != NULL) {
+        g_object_unref(metadata->original_img.pixbuf);
     }
-    if (metadata->image.output != NULL) {
-        g_object_unref(metadata->image.output);
+    if (metadata->output_img.pixbuf != NULL) {
+        g_object_unref(metadata->output_img.pixbuf);
     }
 
     // Load image data
-    metadata->image.original = gdk_pixbuf_new_from_file(filename, NULL);
-    metadata->image.output = gdk_pixbuf_copy(metadata->image.original);
-    if (gdk_pixbuf_get_n_channels(metadata->image.original) == 1)
-        metadata->image.grayscale = TRUE;
-    else
-        metadata->image.grayscale = FALSE;
+    metadata->original_img.pixbuf = gdk_pixbuf_new_from_file(filename, NULL);
+    metadata->output_img.pixbuf = gdk_pixbuf_copy(metadata->original_img.pixbuf);
 }
 
 void save_image(char *filename, AppData *metadata) {
-    gdk_pixbuf_save(metadata->image.output, filename, "jpeg", NULL, NULL);
+    gdk_pixbuf_save(metadata->output_img.pixbuf, filename, "jpeg", NULL, NULL);
 }
 
 void place_image_outwindow(AppData *metadata) {
-    GtkWidget *img = gtk_picture_new_for_pixbuf(metadata->image.output);
-    int width = gdk_pixbuf_get_width(metadata->image.output);
-    int height = gdk_pixbuf_get_height(metadata->image.output);
+    GtkWidget *img = gtk_picture_new_for_pixbuf(metadata->output_img.pixbuf);
+    int width = gdk_pixbuf_get_width(metadata->output_img.pixbuf);
+    int height = gdk_pixbuf_get_height(metadata->output_img.pixbuf);
     gtk_window_set_default_size(GTK_WINDOW(metadata->out_window), width, height);
     gtk_window_set_child(GTK_WINDOW(metadata->out_window), img);
 }
 
 void place_image_srcwindow(AppData *metadata) {
-    GtkWidget *img = gtk_picture_new_for_pixbuf(metadata->image.original);
-    int width = gdk_pixbuf_get_width(metadata->image.original);
-    int height = gdk_pixbuf_get_height(metadata->image.original);
+    GtkWidget *img = gtk_picture_new_for_pixbuf(metadata->original_img.pixbuf);
+    int width = gdk_pixbuf_get_width(metadata->original_img.pixbuf);
+    int height = gdk_pixbuf_get_height(metadata->original_img.pixbuf);
     gtk_window_set_default_size(GTK_WINDOW(metadata->src_window), width, height);
     gtk_window_set_child(GTK_WINDOW(metadata->src_window), img);
 }
@@ -106,18 +102,17 @@ void load_button_click(GtkWidget* widget, AppData *metadata) {
 }
 
 void reset_button_click(GtkWidget* widget, AppData *metadata) {
-    if (metadata->image.original == NULL) {
+    if (metadata->original_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
-    g_object_unref(metadata->image.output);
-    metadata->image.output = gdk_pixbuf_copy(metadata->image.original);
-    metadata->image.grayscale = FALSE;
+    g_object_unref(metadata->output_img.pixbuf);
+    metadata->output_img.pixbuf = gdk_pixbuf_copy(metadata->original_img.pixbuf);
     place_image_outwindow(metadata);
 }
 
 void save_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
@@ -138,110 +133,106 @@ void save_button_click(GtkWidget *widget, AppData *metadata) {
 }
 
 void hflip_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
-    hflip(metadata->image.output);
+    hflip(metadata->output_img.pixbuf);
     place_image_outwindow(metadata);
 }
 
 void vflip_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
-    vflip(metadata->image.output);
+    vflip(metadata->output_img.pixbuf);
     place_image_outwindow(metadata);
 }
 
 void gray_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
-    rgb_to_l(metadata->image.output);
+    rgb_to_l(metadata->output_img.pixbuf);
     place_image_outwindow(metadata);
-    metadata->image.grayscale = TRUE;
 }
 
 void neg_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
-    negative(metadata->image.output);
+    negative(metadata->output_img.pixbuf);
     place_image_outwindow(metadata);
 }
 
 void q_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
-    if (!metadata->image.grayscale) {
-        rgb_to_l(metadata->image.output);
-        metadata->image.grayscale = TRUE;
-    }
+    rgb_to_l(metadata->output_img.pixbuf);
     int q = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(metadata->q_amount));
-    l_quantize(metadata->image.output, q);
+    l_quantize(metadata->output_img.pixbuf, q);
     place_image_outwindow(metadata);
 }
 
 void b_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
     int b = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(metadata->b_amount));
-    brightness(metadata->image.output, b);
+    brightness(metadata->output_img.pixbuf, b);
     place_image_outwindow(metadata);
 }
 
 void c_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
     double c = gtk_spin_button_get_value(GTK_SPIN_BUTTON(metadata->c_amount));
-    contrast(metadata->image.output, c);
+    contrast(metadata->output_img.pixbuf, c);
     place_image_outwindow(metadata);
 }
 
 void rotr_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
-    metadata->image.output = rotate_right90(metadata->image.output);
+    metadata->output_img.pixbuf = rotate_right90(metadata->output_img.pixbuf);
     place_image_outwindow(metadata);
 }
 
 void rotl_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
-    metadata->image.output = rotate_left90(metadata->image.output);
+    metadata->output_img.pixbuf = rotate_left90(metadata->output_img.pixbuf);
     place_image_outwindow(metadata);
 }
 
 void zin_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
-    metadata->image.output = zoom_in(metadata->image.output);
+    metadata->output_img.pixbuf = zoom_in(metadata->output_img.pixbuf);
     place_image_outwindow(metadata);
 }
 
 void zout_button_click(GtkWidget *widget, AppData *metadata) {
-    if (metadata->image.output == NULL) {
+    if (metadata->output_img.pixbuf == NULL) {
         g_print("No image loaded\n");
         return;
     }
     int sx, sy;
-    metadata->image.output = zoom_out(metadata->image.output, sx, sy);
+    metadata->output_img.pixbuf = zoom_out(metadata->output_img.pixbuf, sx, sy);
     place_image_outwindow(metadata);
 }
 
@@ -254,7 +245,7 @@ static void activate(GtkApplication *app, AppData *metadata) {
               *b_amount, *b_button, *c_amount,
               *c_button, *rotr_button, *rotl_button,
               *zin_button, *zout_button;
-    GtkBuilder *builder = gtk_builder_new_from_file("ui.xml");
+    GtkBuilder *builder = gtk_builder_new_from_file("main.ui");
     tool_window = GTK_WIDGET(gtk_builder_get_object(builder, "tools"));
     src_window = GTK_WIDGET(gtk_builder_get_object(builder, "source"));
     out_window = GTK_WIDGET(gtk_builder_get_object(builder, "output"));
@@ -299,6 +290,7 @@ static void activate(GtkApplication *app, AppData *metadata) {
     g_signal_connect(zin_button, "clicked", G_CALLBACK(zin_button_click), metadata);
     g_signal_connect(zout_button, "clicked", G_CALLBACK(zout_button_click), metadata);
 
+
     gtk_widget_show(tool_window);
     gtk_widget_show(src_window);
     gtk_widget_show(out_window);
@@ -310,9 +302,9 @@ int main(int argc, char **argv) {
 
     // Init application state
     AppData metadata;
-    metadata.image.original = NULL;
-    metadata.image.output = NULL;
-    metadata.image.hist = malloc(sizeof(unsigned char)*256);
+    metadata.original_img.pixbuf = NULL;
+    metadata.output_img.pixbuf = NULL;
+    // metadata.image.hist = malloc(sizeof(unsigned char)*256);
 
     app = gtk_application_new("dev.gpl27.fpi.trab1", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), &metadata);
@@ -320,7 +312,7 @@ int main(int argc, char **argv) {
     g_object_unref(app);
 
     // Destroy application state
-    free(metadata.image.hist);
+    // free(metadata.image.hist);
 
     return status;
 }
